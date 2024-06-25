59b06acca360ea7721c8800f5f3a499daf90bf1e
Yash05080 <yash05080@gmail.com>
2024-06-25 23:50:29 +0530

    firabase ios setup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:e_commerce/screens/nav_bar.dart';
 import 'package:e_commerce/screens/splashscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
