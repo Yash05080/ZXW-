f9efa986414d4297f1653c4c722d9219ca0fd3e5
Yash05080 <yash05080@gmail.com>
2024-06-28 15:27:45 +0530

    flutter updrage backup

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,5 +1,5 @@
 import 'dart:js_interop';
-import 'dart:js_interop';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
