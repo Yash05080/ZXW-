be07778e6ff81aa682b20bd87db507707e004e43
Yash05080 <yash05080@gmail.com>
2024-07-02 03:11:14 +0530

    forgot password method implemented using firebase

--- lib/screens/loginPage/forgotPass.dart
+++ lib/screens/loginPage/forgotPass.dart
@@ -1,4 +1,9 @@
+import 'dart:ui';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class ForgetPass extends StatefulWidget {
   const ForgetPass({super.key});
@@ -8,8 +13,149 @@ class ForgetPass extends StatefulWidget {
 }
 
 class _ForgetPassState extends State<ForgetPass> {
+  final _usernameController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _usernameController.dispose();
+    super.dispose();
+  }
+
+  Future passwordReset() async {
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: _usernameController.text.trim());
+    } on FirebaseAuthException catch (e) {
+      print(e);
+
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: Text(
+                e.message.toString(),
+                style: TextStyle(
+                    color: HexColor("C00000"),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
+              backgroundColor: HexColor("FFD078"),
+            );
+          });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: HexColor("FFD078"),
+        backgroundColor: HexColor("080302"),
+      ),
+      body: Stack(children: [
+        Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              color: Colors.black,
+              image: DecorationImage(
+                image: AssetImage("assets/images/wardrobe.jpeg"),
+                fit: BoxFit.cover,
+                opacity: 0.4,
+              )),
+        ),
+        Column(
+          children: [
+            SizedBox(
+              height: 25,
+            ),
+            Text(
+              "dear user, please enter your email \ndown to recieve the reset password link",
+              style: TextStyle(fontSize: 20, color: HexColor("FFD078")),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Container(
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    color: Colors.white12,
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15.0),
+                    child: TextField(
+                      controller: _usernameController,
+                      style: TextStyle(color: HexColor("FFD078")),
+                      cursorColor: HexColor("FFD078"),
+                      decoration: InputDecoration(
+                          suffixIcon: Icon(
+                            Icons.person,
+                            color: HexColor("C00000"),
+                          ),
+                          hintText: "username",
+                          hintStyle: TextStyle(color: Colors.grey),
+                          border: InputBorder.none),
+                    ),
+                  )),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Stack(
+                children: [
+                  // Frosted glass effect
+                  BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                    child: Container(
+                      width: 200,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(
+                          color: Colors.white.withOpacity(0.3),
+                          width: 1.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  // Elevated button on top of the frosted glass effect
+                  SizedBox(
+                    width: 200,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        shadowColor: Colors.transparent,
+                      ),
+                      onPressed: () {
+                        passwordReset();
+
+                        // Handle sign in button press
+                      },
+                      child: Text(
+                        'Reset Password',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ]),
+    );
   }
 }
